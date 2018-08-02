7b27a3fb14f498f421e9eba9d2b9a81aea69d5d5
Tim Untersberger <timuntersberger2@gmail.com>
2018-08-02 23:16:02 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! .

--- static/js/episode.js
+++ static/js/episode.js
@@ -2,8 +2,10 @@ const video = new Plyr("#player");
 
 video.on("play", event => {
   document.getElementById("navbar").hidden = true;
+  document.getElementById("episode-nav").hidden = true;
 });
 
 video.on("pause", event => {
   document.getElementById("navbar").hidden = false;
+  document.getElementById("episode-nav").hidden = false;
 });
